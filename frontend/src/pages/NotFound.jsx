19870221fe3fefe5980c8ce536993a13205e0f40
MUSTAFA Unwala <unwalamustafa26@gmail.com>
2024-06-09 18:26:48 +0530

    first commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p>the page you're looking is not available!!</p>
+    </div>
+  )
+}
+
+export default NotFound
